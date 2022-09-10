19d227af3922a4a7327a99a69824bb0d72a5efc8
zouyuxuan <1311226127@qq.com>
2022-09-10 17:58:39 +0800

    add kitties pallet

---
+++ pallets/kitties/src/lib.rs
@@ -0,0 +1,202 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::pallet_prelude::*;
+	use frame_support::traits::{Randomness,Currency, ReservableCurrency};
+	use frame_system::pallet_prelude::*;
+	use sp_io::hashing::blake2_128;
+	use sp_runtime::traits::{AtLeast32BitUnsigned, Bounded, One};
+
+	#[derive(Encode, Decode, Clone, PartialEq, Eq, Debug, TypeInfo, MaxEncodedLen)]
+	pub struct Kitty(pub [u8; 16]);
+    // 定义token存储
+    type BalanceOf<T> = <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+		type Randomness: Randomness<Self::Hash, Self::BlockNumber>;
+		type KittyIndex: Parameter
+			+ Member
+			+ AtLeast32BitUnsigned
+			+ Default
+			+ Copy
+			+ MaxEncodedLen
+			+ Bounded;
+		type MaxKittiesOwned: Get<u32>;
+        type KittyReserve:Get<BalanceOf<Self>>;
+        type Currency: Currency<Self::AccountId> + ReservableCurrency<Self::AccountId>;
+	}
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	#[pallet::storage]
+	#[pallet::getter(fn next_kitty_id)]
+	pub type NextKittyId<T> = StorageValue<_, <T as Config>::KittyIndex, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn kitties)]
+	pub type Kitties<T> = StorageMap<_, Blake2_128Concat, <T as Config>::KittyIndex, Kitty>;
+
+	// 扩展存储，能得到一个账号拥有的所有kitties
+	#[pallet::storage]
+	#[pallet::getter(fn owner_kitties)]
+	pub type OwnerKitties<T: Config> = StorageMap<
+		_,
+		Blake2_128Concat,
+		T::AccountId,
+		BoundedVec<Kitty, T::MaxKittiesOwned>,
+		ValueQuery,
+	>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn kitty_owner)]
+	pub type KittyOwner<T: Config> = StorageMap<_, Blake2_128Concat, T::KittyIndex, T::AccountId>;
+
+	// 定义事件
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super)fn deposit_event)]
+	pub enum Event<T: Config> {
+		KittyCreated(T::AccountId, T::KittyIndex, Kitty),
+		KittyBred(T::AccountId, T::KittyIndex, Kitty),
+		KittyTransferred(T::AccountId, T::AccountId, T::KittyIndex),
+	}
+
+	// 定义错误
+	#[pallet::error]
+	pub enum Error<T> {
+		InvalidKittyId,
+		NotOwner,
+		SameKittyId,
+		MaxKittyOwned,
+        TokenNotEnough
+	}
+
+	// 设置对外调用方法
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::weight(10_000)]
+		pub fn create(origin: OriginFor<T>) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+            // 检查token数量
+            T::Currency::reserve(&who, T::KittyReserve::get()).map_err(|_| Error::<T>::TokenNotEnough)?;
+			let kitty_id = Self::get_next_id().map_err(|_| Error::<T>::InvalidKittyId)?;
+
+			let dna = Self::random_value(&who);
+			let kitty = Kitty(dna);
+
+			OwnerKitties::<T>::try_mutate(&who, |kitty_vec| kitty_vec.try_push(kitty.clone()))
+				.map_err(|_| <Error<T>>::MaxKittyOwned)?;
+
+			Kitties::<T>::insert(kitty_id, &kitty);
+			KittyOwner::<T>::insert(kitty_id, &who);
+			NextKittyId::<T>::set(kitty_id + One::one());
+
+			// Emit an event.
+			Self::deposit_event(Event::KittyCreated(who, kitty_id, kitty));
+			Ok(())
+		}
+		#[pallet::weight(10_000)]
+		pub fn breed(
+			origin: OriginFor<T>,
+			kitty_id_1: T::KittyIndex,
+			kitty_id_2: T::KittyIndex,
+		) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+            T::Currency::reserve(&who, T::KittyReserve::get()).map_err(|_| Error::<T>::TokenNotEnough)?;
+			// check kitty id
+			ensure!(kitty_id_1 != kitty_id_2, Error::<T>::SameKittyId);
+			let kitty_1 = Self::get_kitty(kitty_id_1).map_err(|_| Error::<T>::InvalidKittyId)?;
+			let kitty_2 = Self::get_kitty(kitty_id_2).map_err(|_| Error::<T>::InvalidKittyId)?;
+
+			// get next id
+			let kitty_id = Self::get_next_id().map_err(|_| Error::<T>::InvalidKittyId)?;
+
+			// selector for breeding
+			let selector = Self::random_value(&who);
+
+			let mut data = [0u8; 16];
+			for i in 0..kitty_1.0.len() {
+				// 0 choose kitty2, and 1 choose kitty1
+				data[i] = (kitty_1.0[i] & selector[i]) | (kitty_2.0[i] & !selector[i]);
+			}
+			let new_kitty = Kitty(data);
+			OwnerKitties::<T>::try_mutate(&who, |kitty_vec| kitty_vec.try_push(new_kitty.clone()))
+				.map_err(|_| <Error<T>>::MaxKittyOwned)?;
+
+			<Kitties<T>>::insert(kitty_id, &new_kitty);
+			KittyOwner::<T>::insert(kitty_id, &who);
+			NextKittyId::<T>::set(kitty_id + One::one());
+
+			Self::deposit_event(Event::KittyCreated(who, kitty_id, new_kitty));
+
+			Ok(())
+		}
+
+		#[pallet::weight(10_000)]
+		pub fn transfer(
+			origin: OriginFor<T>,
+			kitty_id: T::KittyIndex,
+			new_owner: T::AccountId,
+		) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+            // 锁定买家token
+            T::Currency::reserve(&new_owner, T::KittyReserve::get()).map_err(|_| Error::<T>::TokenNotEnough)?;
+			let id = Self::get_kitty(kitty_id).map_err(|_| Error::<T>::InvalidKittyId)?;
+            OwnerKitties::<T>::try_mutate(&who, |owned| {
+				if let Some(index) = owned.iter().position(|kitty| kitty == &id) {
+					owned.swap_remove(index);
+					return Ok(());
+				}
+				Err(())
+			}).map_err(|_| <Error<T>>::NotOwner)?;
+			ensure!(Self::kitty_owner(kitty_id) == Some(who.clone()), Error::<T>::NotOwner);
+
+            // 解锁卖家的冻结
+            T::Currency::unreserve(&who, T::KittyReserve::get());
+
+            <KittyOwner<T>>::insert(kitty_id, &new_owner);
+            
+            OwnerKitties::<T>::try_mutate(&new_owner, |vec| {
+				vec.try_push(id)
+			}).map_err(|_| <Error<T>>::MaxKittyOwned)?;
+
+			
+            
+			Ok(())
+		}
+	}
+
+	// 对内调用方法
+	impl<T: Config> Pallet<T> {
+		fn random_value(sender: &T::AccountId) -> [u8; 16] {
+			let payload = (
+				T::Randomness::random_seed(),
+				&sender,
+				<frame_system::Pallet<T>>::extrinsic_index(),
+			);
+			payload.using_encoded(blake2_128)
+		}
+		fn get_next_id() -> Result<T::KittyIndex, DispatchError> {
+			let kitty_id = Self::next_kitty_id();
+			if kitty_id == T::KittyIndex::max_value() {
+				return Err(Error::<T>::InvalidKittyId.into());
+			}
+			Ok(kitty_id)
+		}
+		fn get_kitty(kitty_id: T::KittyIndex) -> Result<Kitty, ()> {
+			match Self::kitties(kitty_id) {
+				Some(kitty) => Ok(kitty),
+				None => Err(()),
+			}
+		}
+
+		// fn get_owner(kitty_id:T::KittyIndex){
+
+		// }
+	}
+}
